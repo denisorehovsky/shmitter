f4e3257a792f6ca5dcc0867ecc13a8d74035d2f3
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-06-24 09:30:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Shmitter
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+Twitter clone
---
+++ backend/config/urls.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+
+    # Django Admin, use {% url 'admin:index' %}
+    url(settings.ADMIN_URL, admin.site.urls),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    if 'debug_toolbar' in settings.INSTALLED_APPS:
+        import debug_toolbar
+        urlpatterns = [
+            url(r'^__debug__/', include(debug_toolbar.urls)),
+        ] + urlpatterns
