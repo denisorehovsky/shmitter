e8d892353b4524017434d8bf4ff81e7c29114bc3
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-06-24 16:37:29 +0300

    Add Tweet serializer

---
+++ backend/shmitter/tweets/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Tweet
+
+
+class TweetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tweet
+        fields = (
+            'id',
+            'body',
+            'created',
+        )
