66c011970f0f15b4f3605954422d2e39b9b67d56
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-07-06 09:27:09 +0300

    Add login view test

---
+++ backend/tests/authentication/test_views.py
@@ -0,0 +1,23 @@
+from django.core.urlresolvers import reverse
+
+from rest_framework import status
+
+import pytest
+from nose.tools import eq_, ok_
+
+from .. import factories as f
+
+pytestmark = pytest.mark.django_db
+
+
+def test_login(client):
+    user_1 = f.UserFactory.create(password='password')
+    data = {
+        'username_or_email': user_1.email,
+        'password': 'password'
+    }
+    url = reverse('login')
+
+    response = client.post(url, data)
+    eq_(response.status_code, status.HTTP_200_OK)
+    ok_('auth_token' in response.data)
