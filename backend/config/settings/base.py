d3428562426be77803e7c9c6b5c390546409fc81
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-07-11 08:29:08 +0300

    Add action permissions

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -55,6 +55,7 @@ THIRD_PARTY_APPS = [
 # Apps specific for this project go here.
 LOCAL_APPS = [
     'shmitter.authentication',
+    'shmitter.base',
     'shmitter.tweets',
     'shmitter.users',
 ]
