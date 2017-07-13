746f42e42b01b3744e8b6d53b28b50b912da7191
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-07-13 09:22:44 +0300

    Add get_fans service

--- backend/shmitter/likes/services.py
+++ backend/shmitter/likes/services.py
@@ -1,6 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
 
 from shmitter.likes.models import Like
+
+User = get_user_model()
 
 
 def add_like(obj, user):
@@ -34,3 +37,15 @@ def remove_like(obj, user):
         content_type=obj_type, object_id=obj.id, user=user)
     if likes.exists():
         likes.delete()
+
+
+def get_fans(obj):
+    """Get the users which liked an `obj`.
+
+    :param obj: Any Django model instance.
+
+    :return: Queryset of users.
+    """
+    obj_type = ContentType.objects.get_for_model(obj)
+    return User.objects.filter(
+        likes__content_type=obj_type, likes__object_id=obj.id)
--- backend/tests/likes/test_services.py
+++ backend/tests/likes/test_services.py
@@ -1,5 +1,6 @@
 import pytest
 from nose.tools import eq_
+from django_nose.tools import assert_queryset_equal
 
 from shmitter.likes import services
 from .. import factories as f
@@ -26,3 +27,19 @@ def test_add_and_remove_like():
 
     services.remove_like(tweet_1, user_2)
     eq_(tweet_1.likes.count(), 1)
+
+
+def test_get_fans():
+    tweet_1 = f.TweetFactory.create()
+    user_1 = f.UserFactory.create()
+    user_2 = f.UserFactory.create()
+    f.UserFactory.create()
+
+    f.LikeTweetFactory.create(content_object=tweet_1, user=user_1)
+    f.LikeTweetFactory.create(content_object=tweet_1, user=user_2)
+
+    assert_queryset_equal(
+        services.get_fans(tweet_1),
+        [repr(user_1), repr(user_2)],
+        ordered=False
+    )
