5d63c35db954b7befe2a51ba8554d8bff7b6dd74
Denis Orehovsky <denis.orehovsky@gmail.com>
2017-07-19 15:24:02 +0300

    Add login page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '@/views/Home'
+import Login from '@/views/Login'
 
 Vue.use(Router)
 
@@ -11,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
